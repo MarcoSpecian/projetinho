f5a15dfeaffe1d959ae35feb57400d07d7666c99
Gunter6256 <marco.specian94@gmail.com>
2025-03-30 15:08:54 -0300

    Criando as Controllers e seus códigos

---
+++ projetinho/Controllers/ProdutoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projetinho.Repositorio;
+using projetinho.Models;
+using projeto1.Repositorio;
+
+namespace projetinho.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //contrutor
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public ProdutoController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+                //possivel erro
+            }
+            return View(usuario);
+        }
+    }
+}
---
+++ projetinho/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using projetinho.Repositorio;
+using projetinho.Models;
+using projeto1.Repositorio;
+
+namespace projetinho.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //contrutor
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha == senha)
+            {
+                //Autenticação bem-sucedida
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senha inválidos");
+            return View();
+
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
+        }
+    }
+}
