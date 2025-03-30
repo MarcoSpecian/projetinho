4134faa09e338452329b8313a5e8c106b486e53a
MarcoSpecian <marco.specian94@gmail.com>
2025-03-30 13:53:26 -0300

    Criando o projetinho

---
+++ projetinho/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projetinho.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
